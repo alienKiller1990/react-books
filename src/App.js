638d3a38e5509212f47913ebd5dd6ebc8fd7c677
Pavel Che <alien.killer3@bk.ru>
2021-10-08 11:48:15 +0300

    connected a menu from semantic-ui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react'
 import { connect } from 'react-redux';
 import { setBooks } from './actions/books';
+import { Menu } from 'semantic-ui-react'
 
 class App extends React.Component {
 
@@ -13,23 +14,57 @@ class App extends React.Component {
   }
 
   render() {
-    const { books } = this.props
+    const { books, isReady } = this.props
     return (
-      <ul>
-        {
-          !books
-            ? 'Загрузка..'
-            : books.map(book => (
-              <li><b>{book.title}</b> - {book.author}</li>
-            ))
-        }
-      </ul>
+      <>
+        <Menu>
+          <Menu.Item
+            name='browse'
+            onClick={this.handleItemClick}
+          >
+            Browse
+          </Menu.Item>
+
+          <Menu.Item
+            name='submit'
+            onClick={this.handleItemClick}
+          >
+            Submit
+          </Menu.Item>
+
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='signup'
+              onClick={this.handleItemClick}
+            >
+              Sign Up
+            </Menu.Item>
+
+            <Menu.Item
+              name='help'
+              onClick={this.handleItemClick}
+            >
+              Help
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <ul>
+          {
+            !isReady
+              ? 'Загрузка..'
+              : books.map(book => (
+                <li><b>{book.title}</b> - {book.author}</li>
+              ))
+          }
+        </ul>
+      </>
     )
   }
 }
 
 const mapStateToProps = ({ books }) => ({
-  books: books.items
+  books: books.items,
+  isReady: books.isReady
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css'
 
 import createStore from './store';
 
