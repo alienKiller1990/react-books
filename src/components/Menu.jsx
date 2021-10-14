0a57e7ea4aba924a25119d2f4b678496813b702a
Pavel Che <alien.killer3@bk.ru>
2021-10-14 19:13:10 +0300

    implemented remove from cart

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,30 +1,38 @@
-import { Menu } from 'semantic-ui-react'
 import React from 'react'
+import { Menu, Popup, List, Button, Image } from 'semantic-ui-react'
 
-const MenuComponent = ({ totalPrice, count }) => {
+const CartComponent = ({ title, id, image, removeFromCart }) => {
+    return (
+        <List selection divided verticalAlign='middle'>
+            <List.Item>
+                <List.Content floated='right'>
+                    <Button onClick={removeFromCart.bind(this, id)} color="red">Удалить</Button>
+                </List.Content>
+                <Image avatar src={image} />
+                <List.Content>{title}</List.Content>
+            </List.Item>
+        </List>
+    )
+}
+
+
+const MenuComponent = ({ totalPrice, count, items }) => {
     return (
         <Menu>
-            <Menu.Item
-                name='browse'
-            // onClick={this.handleItemClick}
-            >
-                Магазин книг
-            </Menu.Item>
+            <Menu.Item name='browse'>Магазин книг</Menu.Item>
 
             <Menu.Menu position='right'>
-                <Menu.Item
-                    name='signup'
-                // onClick={this.handleItemClick}
-                >
-                    Итого: &nbsp; <b>{totalPrice}</b>&nbsp; руб.
-                </Menu.Item>
+                <Menu.Item name='signup'>Итого: &nbsp; <b>{totalPrice}</b>&nbsp; руб.</Menu.Item>
 
-                <Menu.Item
-                    name='help'
-                // onClick={this.handleItemClick}
-                >
-                    Корзина ( <b>{count}</b> )
-                </Menu.Item>
+                <Popup
+                    trigger={
+                        <Menu.Item name='help'>Корзина ( <b>{count}</b> )</Menu.Item>
+                    }
+                    content={items.map(book => <CartComponent {...book}/>)}
+                    on='click'
+                    hideOnScroll
+                />
+
             </Menu.Menu>
         </Menu>
     )
--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -2,10 +2,12 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as cartActions from '../actions/cart';
 import Menu from '../components/Menu';
+import unionBy from 'lodash/unionBy'
 
 const mapStateToProps = ({ cart }) => ({
     totalPrice: cart.items.reduce((total, book) => total + book.price, 0),
-    count: cart.items.length
+    count: cart.items.length,
+    items: unionBy(cart.items, obj => obj.id),
 })
 
 const mapDispatchToProps = dispatch => ({
